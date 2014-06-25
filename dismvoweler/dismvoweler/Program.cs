a2b0b04fff58ca644451300c01a6121107b27917
Phoenix Henke <phoenixhenke098>
2014-06-25 07:51:24 -0600

    new changes

--- dismvoweler/dismvoweler/Program.cs
+++ dismvoweler/dismvoweler/Program.cs
@@ -10,7 +10,30 @@ namespace dismvoweler
     {
         static void Main(string[] args)
         {
-            Console.ReadKey();
+            string vowels = "aeiou";
+
+            Console.WriteLine("Insert string to be dismvoweler");
+            string input = Console.ReadLine();
+            char[] removed = new char[50];
+            int count = 0;
+            for (int p = 0; p < input.Length; p++)
+            {
+                if (input[p] == 'a' || input[p] == 'e' || input[p] == 'i' || input[p] == 'o' || input[p] == 'u')
+                    removed[count] = input[p];
+                count++;
+            }
+             string step1 = input.Replace(" ",string.Empty);
+             step1 = step1.Replace("a", string.Empty);
+             step1 = step1.Replace("e", string.Empty);
+             step1 = step1.Replace("i", string.Empty);
+             step1 = step1.Replace("o", string.Empty);
+             step1 = step1.Replace("u", string.Empty);
+
+             Console.WriteLine("output: " + step1 + "\n");
+             Console.WriteLine("and");
+            Console.WriteLine("removed: " + new string(removed));
+
+            Console.ReadKey(true);
         }
     }
 }
